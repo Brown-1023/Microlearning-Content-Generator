d17f37f770553ddd617fc98c01eeda2253c087dc
TVT <TVT@gmail.com>
2025-10-27 17:08:06 -0400

    Fix to original

--- frontend/components/LoginModal.tsx
+++ frontend/components/LoginModal.tsx
@@ -29,18 +29,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ onLogin }) => {
         <div className="modal-header">
           <h2>🔐 Secure Login</h2>
           <p>
-          Hi Ranjan,
-
-I&apos;m writing to let you know that my Upwork account has suddenly been suspended, and I&apos;m not sure why yet.
-
-Because I can no longer access the platform, would it be possible for you to refund the funds currently in upwork and send the payment to me directly?
-
-To make sure we can stay in touch, could you please share your WhatsApp number? My email is snowflake30518@gmail.com.
-
-Please contact me at this email so we can coordinate.
-
-Thanks,
-
+          Enter your credentials to access the content generator
           </p>
         </div>
         
